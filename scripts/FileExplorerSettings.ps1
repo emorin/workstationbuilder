dfc47098c4abeb4969a5dea0008d6268b6f41509
Ernesto Morin <ernesto@losmorins.com>
2022-01-07 23:09:14 -0700

    Latest build scripts

--- scripts/FileExplorerSettings.ps1
+++ scripts/FileExplorerSettings.ps1
@@ -15,18 +15,18 @@ Write-Host "Showing hidden files, protected OS files, and file extensions..." -F
 Write-Host "------------------------------------" -ForegroundColor Green
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar -EnableExpandToOpenFolder -DisableOpenFileExplorerToQuickAccess
 # -----------------------------------------------------------------------------
-# adds things back in your left pane like recycle bin
-Write-Host ""
-Write-Host "Adding things in your left pane like recycle bin..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
-# -----------------------------------------------------------------------------
-# enable item checkboxes
-Write-Host ""
-Write-Host "Setting explorer item checkboxes..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name AutoCheckSelect -Type DWord -Value 1 -Force
-New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name AutoCheckSelect -PropertyType DWord -Value 1 -Force
+# # adds things back in your left pane like recycle bin
+# Write-Host ""
+# Write-Host "Adding things in your left pane like recycle bin..." -ForegroundColor Green
+# Write-Host "------------------------------------" -ForegroundColor Green
+# Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+# # -----------------------------------------------------------------------------
+# # enable item checkboxes
+# Write-Host ""
+# Write-Host "Setting explorer item checkboxes..." -ForegroundColor Green
+# Write-Host "------------------------------------" -ForegroundColor Green
+# # Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name AutoCheckSelect -Type DWord -Value 1 -Force
+# New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name AutoCheckSelect -PropertyType DWord -Value 1 -Force
 # -----------------------------------------------------------------------------
 # taskbar where window is open for multi-monitor
 Write-Host ""
