c39775e42ab32334102407254fd5b902a197df0b
Ernesto Morin <ernesto@losmorins.com>
2023-05-25 16:16:31 -0600

    Update CommonDevTools.ps1

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -9,7 +9,7 @@ Write-Host "------------------------------------" -ForegroundColor Gray
 Write-Host ""
 Write-Host "Installing Git for Windows..." -ForegroundColor Green
 #choco upgrade git.install --cacheLocation="$ChocoCachePath" --params "/GitAndUnixToolsOnPath /NoGitLfs /SChannel /NoAutoCrlf /WindowsTerminal"
-choco upgrade git.install --cacheLocation="$ChocoCachePath" --params "'/GitAndUnixToolsOnPath /WindowsTerminal /NoAutoCrlf'"
+choco upgrade git.install --cacheLocation="$ChocoCachePath" --params "'/GitAndUnixToolsOnPath /NoGitLfs /SChannel /WindowsTerminal /NoAutoCrlf'"
 RefreshEnv
 Update-SessionEnvironment #refreshing env due to Git install
 
