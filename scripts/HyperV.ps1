3b1e0befc7a1060a881584bbdd9dd64f1ebd2c55
Ernesto Morin <ernesto@losmorins.com>
2023-05-25 15:38:13 -0600

    Update HyperV.ps1

--- scripts/HyperV.ps1
+++ scripts/HyperV.ps1
@@ -6,9 +6,9 @@ Write-Host ""
 Write-Host "Installing HyperV..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 
-Enable-WindowsOptionalFeature -Online -FeatureName "Containers" -NoRestart -NoRestart -WarningAction SilentlyContinue -All
-Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -NoRestart -NoRestart -WarningAction SilentlyContinue -All
+Enable-WindowsOptionalFeature -Online -FeatureName "Containers" -NoRestart -WarningAction SilentlyContinue -All
+Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -NoRestart -WarningAction SilentlyContinue -All
 Enable-WindowsOptionalFeature -Online -FeatureName "Containers-DisposableClientVM" -All
-Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-All" -NoRestart -NoRestart -WarningAction SilentlyContinue -All
+Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-All" -NoRestart -WarningAction SilentlyContinue -All
 
 RefreshEnv
